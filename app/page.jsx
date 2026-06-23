dd2dcf4c11f0f51cf0a3a55ae985f4846c02cf3f
MahdiArabpour <mahdiarabpour98@gmail.com>
2026-06-23 17:55:43 +0330

    initial project

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,12 @@
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <div className="size-96 bg-green-500 flex-center">aa</div>
+    </main>
+  );
 }
